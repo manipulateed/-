46c96a1ad2b08f1cc639768764cb8ee814ce8dd4
mapulateed <jimmy911116@gmial.com>
2024-05-07 15:18:57 +0800

    flutter

---
+++ view/lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:mindpal_flutter_app/constants/routes.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var loginState = prefs.getString("loginState");
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: loginState == null ? Routes.first : Routes.baseview,
+    routes: Routes.getRoutes(),
+  ));
+}
+
+
+
